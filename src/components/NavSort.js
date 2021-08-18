af8962ece32ac4ba689bae86e7a00292ba23d982
Claire(Saeeun) Kang <ckang23@my.bcit.ca>
2021-08-18 14:11:57 -0700

    Update NavSort
    updating  NavSort for sort part

--- src/components/NavSort.js
+++ src/components/NavSort.js
@@ -1,8 +1,24 @@
+import { NavLink } from 'react-router-dom';
+
 function NavSort() {
     return (
-        <div>
+        <nav className = "nav-sort">
+            <ul>
+                <li>
+                    <NavLink to="/sort/popular">Popular</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/sort/top-rated">Top Rated</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/sort/now-playing">Now Playing</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/sort/upcoming">Upcoming</NavLink>
+                </li>
+            </ul>
             
-        </div>
+        </nav>
     )
 }
 
