9ef0d4284567354b30de5672b85fc02ffb8ecd99
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-18 14:00:28 -0700

    Update AppRouter.js

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,14 +3,22 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import PageHome from '../pages/PageHome';
 import PageAbout from '../pages/PageAbout';
-import PageFavs from '../pages/PageFavs';
+import PageFavorites from '../pages/PageFavorites';
 import PageSingleMovie from '../pages/PageSingleMovie';
 
 function AppRouter() {
   return (
+    <Router>
       <div className="App">
-        
+        <Header />
+        <main>
+          <Switch>
+
+          </Switch>
+        </main>
+        <Footer />
       </div>
+    </Router>
   );
 }
 
