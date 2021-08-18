5e935191d043a212b84fad406ad4197b9daa7437
tiffanyfy <tiffanyyiu3@gmail.com>
2021-08-18 14:44:25 -0700

    Update PageAbout.js

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,8 +1,11 @@
 function PageAbout() {
     return (
-        <div>
-            
-        </div>
+        <section className="about-page">
+            <h2>About Movie Club</h2>
+            <p>Movie Club is an online movie database that lets users browse movies based on popularity, rating and release date. The platform also allows registered users to keep track of movies they love by adding films to their favorites list.</p>
+            <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+            {/* add TMDB logo here */}
+        </section>
     )
 }
 
