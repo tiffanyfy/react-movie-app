68f3ad3d4f2a6da1b3b52549f45af2b04c41c677
tiffanyfy <tiffanyyiu3@gmail.com>
2021-09-10 20:52:13 -0700

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,19 @@
-function MovieCard() {
+import { Link } from 'react-router-dom';
+import noPoster from '../images/no-movie-poster.jpg';
+
+function MovieCard({ movieObj }) {
     return (
-        <div>
-            
+        <div className="movie-card">
+            <div className="movie-poster">
+                {movieObj.poster_path === null ?
+                    <img src={noPoster} alt="No poster" /> :
+                    <img src={`https://image.tmdb.org/t/p/w500/${movieObj.poster_path}`} alt={movieObj.title} />
+                }
+            </div>
+            <div className="movie">
+                <h3>{movieObj.title}</h3>
+                <Link to={`/movie/${movieObj.id}`}>More Info</Link>
+            </div>
         </div>
     )
 }
