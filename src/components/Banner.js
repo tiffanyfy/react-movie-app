62648e300838edc23d43ce6cf4519622a7ceb326
Claire(Saeeun) Kang <ckang23@my.bcit.ca>
2021-09-12 00:55:54 -0700

    Banner(still troubleshooting)
    Still troubleshooting Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,9 @@ import noPoster from '../images/no-movie-poster.jpg';
 
 function Banner( movieObj ) {
     // console.log(movieObj.movieObj[0]);
+    // console.log(movieObj)
+
+
 
 
     // let randomNum = Math.floor(Math.random() * movieObj.movieObj.length);
@@ -9,19 +12,17 @@ function Banner( movieObj ) {
     // let movieDisplayed = movieObj.movieObj[randomNum];
 
 
-
-
     return (
         <div></div>
         // <div className="banner-container">
         //     <img src={`https://image.tmdb.org/t/p/original/${movieDisplayed.backdrop_path}`} alt="placeholder" />
         //     <div className="banner-container" >
-        //         <h2 class="banner-title">{movieDisplayed.title}</h2>
-        //         <h3 class="banner-release-date">{movieDisplayed.release_date}</h3>
-        //         <p class="banner-overview">{movieDisplayed.overview}</p>
+        //         <h2 class="banner-title">{ movieDisplayed && movieDisplayed.title}</h2>
+        //         <h3 class="banner-release-date">{ movieDisplayed && movieDisplayed.release_date}</h3>
+        //         <p class="banner-overview">{ movieDisplayed && movieDisplayed.overview}</p>
 
         //         {/* path to the movie page (use movie id) */}
-        //         <a href={`https://api.themoviedb.org/3/movie/${movieDisplayed.id}?&language=en-US&page=1`}>Watch Now</a>
+        //         <a href="">Watch Now</a>
 
         //     </div>
 
@@ -31,3 +32,29 @@ function Banner( movieObj ) {
 }
 
 export default Banner;
+// function Banner({ movieObj }) {
+//     let randomNum = Math.floor(Math.random() * movieObj && movieObj.length);
+
+//     let movieDisplayed = movieObj && movieObj[randomNum];
+
+
+//     return (
+//         <div className="banner-container">
+//             <img src={`https://image.tmdb.org/t/p/original/${movieDisplayed && movieDisplayed.backdrop_path}`} alt="placeholder" />
+//             <div className="banner-container" >
+//                 <h2 class="banner-title">{movieDisplayed && movieDisplayed.title}</h2>
+//                 <h3 class="banner-release-date">{movieDisplayed && movieDisplayed.release_date}</h3>
+//                 <p class="banner-overview">{movieDisplayed && movieDisplayed.overview}</p>
+
+
+//                 <a href="">Watch Now</a>
+
+//             </div>
+
+            
+//         </div>
+//     )
+// }
+
+// export default Banner;
+
