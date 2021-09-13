dc3021df2d3490964f6a5947eb8db5047068521f
tiffanyfy <tiffanyyiu3@gmail.com>
2021-09-13 04:43:33 -0700

    Update movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,23 +3,53 @@ import noPoster from '../images/no-movie-poster.jpg';
 import addFavorite from '../images/add-favorite.png';
 import removeFavorite from '../images/remove-favorite.png';
 import {NavLink} from 'react-router-dom';
+import { favMovies } from '../globals/globals';
 
 
 function MovieCard({ movieObj }) {
+    
+    function addFav() {
 
-    function goSinglePage() {
-        // 1. Redirect to movie singles page
-        // 2. Pass movieObj to that page through props
-        console.log(`You clicked: ${movieObj.title}`);
-        // <SingleMovie />
-        // <NavLink to={`/movie/:${movieObj.id}`}></NavLink>
-        <NavLink to="/about">About</NavLink>
+        if (favMovies.includes(movieObj.id)) {
+            let position = favMovies.indexOf(movieObj.id)
+            favMovies.splice(position, 1)
+            localStorage.setItem("favMovies", JSON.stringify(favMovies))
+        } else {
+            favMovies.push(movieObj.id);
+            localStorage.setItem("favMovies", JSON.stringify(favMovies))
+        }
+        
+        // if (localStorage.getItem("favMovies") !== null) {
+        //     let favMovies = []
+        // } else {
 
+        //     if (favMovies.includes(movieObj.id)) {
+        //         let pos = favMovies.indexOf(movieObj.id)
+        //         favMovies.splice(pos, 1)
+        //         localStorage.setItem("favMovies", JSON.stringify(favMovies))
+        //     }
+        //     // Pull existing array (favorite movies) from localStorage
+        //     // bc local storage only stores strings
+        //     let stringData = localStorage.getItem("favMovies")
+        //     // Parse converts strings into og data type -> taking string and converting into an array
+        //     let favMovies = JSON.parse(stringData)
+        // }
+        // // TO DO:
+        // // If id already exists, remove from array
 
+        
+        // // Add movie to array
+        // favMovies.push(movieObj.id)
+        // // Save to localStorage
+        // // Stringify converts array into string
+        // // SetItem to save
+        // localStorage.setItem("favMovies", JSON.stringify(favMovies))
+
+        
     }
 
     return (
-        <div className="movie-card" onClick={goSinglePage}>
+        <div className="movie-card">
             <div className="movie-poster">
                 {/* If no poster, then render the no poster image */}
                 {/* If there is a movie poster, then output using movie id (url parameter) */}
@@ -34,7 +64,8 @@ function MovieCard({ movieObj }) {
                 <p>Rating: {movieObj.vote_average}</p>
                 <Link to={`/movie/${movieObj.id}`}>More Info</Link>
                 {/* Will need to modify code below to add/remove favs */}
-                <div className="add-favorite">
+                {/* Event handler to add fav when clicked - go to addFav function */}
+                <div className="add-favorite" onClick={addFav}>
                     <img src={addFavorite} alt="Add heart icon" />
                 </div>
                 <div className="remove-favorite">
--- src/pages/PageFavorites.js
+++ src/pages/PageFavorites.js
@@ -1,7 +1,15 @@
 function PageFavs() {
+    
+
+    // Save favorited movies into local storage
+
+    // Get favMovies
+    let displayFavs = JSON.parse(localStorage.getItem("favMovies"))
+    console.log(displayFavs)
+    
     return (
         <div>
-            
+            <h1>Favorite Movies</h1>
         </div>
     )
 }
