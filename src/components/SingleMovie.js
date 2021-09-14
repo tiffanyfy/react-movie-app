c589aea49d1120e0936de798363c81a7c0771be9
Claire(Saeeun) Kang <ckang23@my.bcit.ca>
2021-09-14 15:59:33 -0700

    SingleMovie page styles

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -3,24 +3,25 @@ import addFavorite from '../images/add-favorite.png';
 
 function SingleMovie({ movieObj }) {
 
-
+    // {backgroundImage: movieObj.backdrop_path && `url(https://image.tmdb.org/t/p/original/${movieObj.backdrop_path})`}
     return (
         <div className="single-movie" style={{backgroundImage: movieObj.backdrop_path && `url(https://image.tmdb.org/t/p/original/${movieObj.backdrop_path})`}}>
+            <div className="single-movie-background-image" ></div>
 
             {/* reference page for the path */}
             {/* https://developers.themoviedb.org/3/getting-started/images */}
             <div className="single-movie-container" >
             
                 <div className="single-movie-content" >
-                    <div className="single-movie-poster">
-                        {movieObj.poster_path === null? <img src={noPoster} alt="no poster image" /> : <img src={`https://image.tmdb.org/t/p/original${movieObj.poster_path}`} alt={movieObj.title} />}
-                    </div>
+                    {/* <div className="single-movie-poster"> */}
+                        {movieObj.poster_path === null? <img src={noPoster} alt="no poster image" /> : <img src={`https://image.tmdb.org/t/p/original${movieObj.poster_path}`} alt={movieObj.title} id='single-movie-poster' />}
+                    {/* </div> */}
                     <div className="single-movie-info">
                         <h2 id="single-movie-title">{movieObj.title}</h2>
                         <div className="single-movie-info-header">
                             <p id="single-movie-release-date">{movieObj.release_date}</p>
                         
-                             <img src={addFavorite} alt="add favorite" />
+                             <img src={addFavorite} alt="add favorite" id="single-movie-fav-btn"/>
                         </div>
                         
                         <p id="single-movie-overview">{movieObj.overview}</p>
