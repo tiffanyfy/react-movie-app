c85dff81469d6a5206809a93399226c7e724d0e1
tiffanyfy <tiffanyyiu3@gmail.com>
2021-09-17 14:19:54 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 const Footer = () => {
     return (
+        
         <footer>
             <p>&copy; 2021 Aaron Lee, Claire Kang, Tiffany Yiu | For Educational Purposes Only</p>
         </footer>
