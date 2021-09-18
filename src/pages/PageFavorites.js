c9252ca172786182b35f274e67e1a9d9a0956903
tiffanyfy <tiffanyyiu3@gmail.com>
2021-09-17 22:48:41 -0700

    Update favorites

--- src/pages/PageFavorites.js
+++ src/pages/PageFavorites.js
@@ -2,50 +2,62 @@ import { useEffect, useState } from 'react';
 import { API_TOKEN } from '../globals/globals';
 import Movies from '../components/Movies';
 
-
-
 function PageFavs() {
     
-    let displayFavs = JSON.parse(localStorage.getItem("favMovies")) // get localStorage fav movies array
-    const [favMovies, setFavMovies] = useState(displayFavs)         // create state for favMovies array
-    const [movieObjArr, setMovieObjArr] = useState([])
+    // let displayFavs = JSON.parse(localStorage.getItem("favMovies")) // get localStorage fav movies array
+    // const [savedMovies, setSavedMovies] = useState(JSON.parse(localStorage.getItem("favMovies")))         // create state for favMovies array
+    const savedMovies = JSON.parse(localStorage.getItem("favMovies"))
 
-    // Build array of movie objects from localStorage ids using fetch
-    useEffect(() => {
-
-        displayFavs.map(async (id) => {
-            let movieList = displayFavs;
-            const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?&language=en-US&page=1`, {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer '+ API_TOKEN 
-                }
-                
-            });
-            let rawMovieData = await res.json();
-            movieList.push(rawMovieData)
-            console.log(movieList)
-
-            setMovieObjArr(movieList)
-
-        })
-
-    }, [favMovies])
-    // const globalStateAndActions = useGlobal();
-    // const globalState = globalStateAndActions[0];
-
-    // useEffect(()=>{
-    //     document.title = ``
-    // })
-    
     return (
         <div>
-            {/* if movieObjArr is empty ... */}
-            {movieObjArr === []? <h1>You have no favorite movies...</h1> : <Movies movieData={movieObjArr} />}
-            
+            <h1>Fav Movies</h1>
+            <Movies movieData={savedMovies} />
         </div>
     )
 }
+
+// function PageFavs() {
+    
+//     let displayFavs = JSON.parse(localStorage.getItem("favMovies")) // get localStorage fav movies array
+//     const [favMovies, setFavMovies] = useState(displayFavs)         // create state for favMovies array
+//     const [movieObjArr, setMovieObjArr] = useState([])
+
+//     // Build array of movie objects from localStorage ids using fetch
+//     useEffect(() => {
+
+//         displayFavs.map(async (id) => {
+//             let movieList = displayFavs;
+//             const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?&language=en-US&page=1`, {
+//                 headers: {
+//                     'Accept': 'application/json',
+//                     'Content-Type': 'application/json',
+//                     'Authorization': 'Bearer '+ API_TOKEN 
+//                 }
+                
+//             });
+//             let rawMovieData = await res.json();
+//             movieList.push(rawMovieData)
+//             console.log(movieList)
+
+//             setMovieObjArr(movieList)
+
+//         })
+
+//     }, [favMovies])
+//     // const globalStateAndActions = useGlobal();
+//     // const globalState = globalStateAndActions[0];
+
+//     // useEffect(()=>{
+//     //     document.title = ``
+//     // })
+    
+//     return (
+//         <div>
+//             {/* if movieObjArr is empty ... */}
+//             {movieObjArr === []? <h1>You have no favorite movies...</h1> : <Movies movieData={movieObjArr} />}
+            
+//         </div>
+//     )
+// }
 
 export default PageFavs;
