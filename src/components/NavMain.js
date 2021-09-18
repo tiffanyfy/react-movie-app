c8d3bfe466700980ee91c40d11d8ce96c86b580a
tiffanyfy <tiffanyyiu3@gmail.com>
2021-09-18 00:42:23 -0700

    Update NavMain.js

--- src/components/NavMain.js
+++ src/components/NavMain.js
@@ -1,22 +1,13 @@
 import {NavLink} from 'react-router-dom';
 import { useState } from 'react';
+import Hamburger from 'hamburger-react';
 
 function NavMain() {
-
-    // const [toggle, setToggle] = useState(false);
-
-    // function toggleFunction(){
-    //     setToggle(true);
-    // }
     
-    
-
-    // hamburger.on('click', function functionname() {
-    //     setToggle('true')
-    // } )
 
     return (
         <nav className="site-navigation" id="site-navigation">
+            {/* <Hamburger toggled={isOpen} toggle={setOpen} /> */}
 
             <button class="menu-toggle" aria-controls="header-menu" aria-expanded="false" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
