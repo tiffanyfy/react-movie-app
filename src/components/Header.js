d7f86ce9f606ff95c409ff9d3a7117c6bfb8880d
tiffanyfy <tiffanyyiu3@gmail.com>
2021-09-18 00:42:32 -0700

    Added site logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom';
 import NavMain from './NavMain';
+import logo from "../images/movie-logo.png";
 
 function Header() {
     return (
         <header className="site-header">
-            <h1><Link to="/">Movie Club</Link></h1>
+            {/* <h1><Link to="/">Movie Club</Link></h1> */}
+            <Link to="/"><img src={logo} alt="back to top button" /></Link>
             <NavMain />
         </header>
     )
